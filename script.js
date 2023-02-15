5d687a0acc6cdc0317f09a2a31f053ada2a2ed23
Steven Ried <70533094+stevenried@users.noreply.github.com>
2023-02-15 12:52:48 -0500

    Add script for show-nav and content rotation.

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container')
+const openButton = document.getElementById('open')
+const closeButton = document.getElementById('close')
+
+openButton.addEventListener('click', () => container.classList.add('show-nav'))
+
+closeButton.addEventListener('click', () =>
+  container.classList.remove('show-nav')
+)
